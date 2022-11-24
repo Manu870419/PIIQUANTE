2851940e5fdc275da0488425afa85c282a1260cc
BEGHIN <beghinmanu870419@gmail.com>
2022-11-24 21:20:43 +0100

    Installation bcrypt et password Hashing

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,13 +1,24 @@
-const User = require("../mongo").User
+const {User} = require("../mongo")
+const bcrypt = require("bcrypt")
 
-function createUser(req, res) {
+async function createUser(req, res) {
     const {email, password} = req.body
-    const user = new User({ email, password })
+
+    const hashedPassword = await hashPassword(password)
+
+    console.log('passeword:', password)
+    console.log('hasedPassword:', hashedPassword)
+    const user = new User({ email, password:hashedPassword })
 
     user
         .save()
         .then(() => res.send({ message: "utilisateur enregistré !" }))
         .catch(err => console.log("User pas enregistré", err))
+}
+
+function hashPassword(password) {
+    const saltRounds = 10;
+    return bcrypt.hash(password,saltRounds)
 }
 
 module.exports = {createUser}
